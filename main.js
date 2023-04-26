d9911e694f861bf1f5183b0405ab3e10c1ab77ba
Teemu Kalmari <teemu.kalmari@student.lab.fi>
2023-04-26 11:05:31 +0300

    adding feature that user can put his own csv file from web to be parsed

--- main.js
+++ main.js
@@ -5,20 +5,47 @@ const cors = require('cors');
 const app = express();
 const port = 5500;
 const csvFilePath = 'data.csv';
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 // cors
 app.use(cors())
 
+app.post('/upload', upload.single('file'), (req, res, next) => {
+    const results = [];
+    const csvFilePath = req.file.path;
+
+    const readStream = fs.createReadStream(csvFilePath);
+
+    // Handle file read errors
+    readStream.on('error', (err) => {
+        console.error(err);
+        next(err);
+    });
+
+    readStream
+        .pipe(csv())
+        .on('data', (data) => results.push(data))
+        .on('end', () => {
+            res.send(results);
+        })
+        .on('error', (err) => next(err));
+});
+
+
 // Handle API requests
 app.get('/', (req, res, next) => {
-  const results = [];
-  
-  // Check if file exists
-  if (!fs.existsSync(csvFilePath)) {
-    const err = new Error('File not found');
-    err.status = 404;
-    return next(err);
-  }
+    const results = [];
+
+    // Get the CSV file path from the query parameter
+    const csvFilePath = req.query.path || 'data1.csv';
+
+    // Check if file exists
+    if (!fs.existsSync(csvFilePath)) {
+        const err = new Error('File not found');
+        err.status = 404;
+        return next(err);
+    }
 
   const readStream = fs.createReadStream(csvFilePath);
 
@@ -48,3 +75,11 @@ app.use((err, req, res, next) => {
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+
+
+
+
+
+
+
