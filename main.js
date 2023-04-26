57b7afd98d5930ee2ea1130bf8bb9f9970bd07ce
Teemu Kalmari <teemu.kalmari@student.lab.fi>
2023-04-26 14:17:58 +0300

    update 1.5.0

--- main.js
+++ main.js
@@ -1,85 +1,46 @@
-const express = require('express');
-const csv = require('csv-parser');
-const fs = require('fs');
-const cors = require('cors');
+
+
+const express = require("express");
+const csv = require("csv-parser");
+const fs = require("fs");
+const cors = require("cors");
 const app = express();
 const port = 5500;
-const csvFilePath = 'data.csv';
-const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 // cors
-app.use(cors())
+app.use(cors());
 
-app.post('/upload', upload.single('file'), (req, res, next) => {
-    const results = [];
-    const csvFilePath = req.file.path;
+let csvData = [];
 
-    const readStream = fs.createReadStream(csvFilePath);
-
-    // Handle file read errors
-    readStream.on('error', (err) => {
-        console.error(err);
-        next(err);
-    });
-
-    readStream
-        .pipe(csv())
-        .on('data', (data) => results.push(data))
-        .on('end', () => {
-            res.send(results);
-        })
-        .on('error', (err) => next(err));
-});
-
-
-// Handle API requests
-app.get('/', (req, res, next) => {
-    const results = [];
-
-    // Get the CSV file path from the query parameter
-    const csvFilePath = req.query.path || 'data1.csv';
-
-    // Check if file exists
-    if (!fs.existsSync(csvFilePath)) {
-        const err = new Error('File not found');
-        err.status = 404;
-        return next(err);
-    }
+app.post("/upload", upload.single("file"), (req, res, next) => {
+  const results = [];
+  const csvFilePath = req.file.path;
 
   const readStream = fs.createReadStream(csvFilePath);
 
   // Handle file read errors
-  readStream.on('error', (err) => {
+  readStream.on("error", (err) => {
     console.error(err);
     next(err);
   });
 
   readStream
     .pipe(csv())
-    .on('data', (data) => results.push(data))
-    .on('end', () => {
+    .on("data", (data) => results.push(data))
+    .on("end", () => {
+      csvData = results;
       res.send(results);
     })
-    .on('error', (err) => next(err));
+    .on("error", (err) => next(err));
 });
 
-// Handle file not found errors
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(err.status || 500).send({
-    error: err.message || 'Internal Server Error'
-  });
+app.get("/", (req, res) => {
+  res.json(csvData);
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${ port } `);
 });
-
-
-
-
-
-
-
 
--- web/api.js
+++ web/api.js
@@ -1,20 +1,21 @@
+
 function getData() {
-  return fetch('http://localhost:5500')
-    .then(response => response.json())
-    .then(data => {
-      const table = document.getElementById('csvTable');
+  return fetch("http://localhost:5500")
+    .then((response) => response.json())
+    .then((data) => {
+      const table = document.getElementById("csvTable");
       const headers = Object.keys(data[0]);
-      const headerRow = document.createElement('tr');
-      headers.forEach(headerText => {
-        const header = document.createElement('th');
+      const headerRow = document.createElement("tr");
+      headers.forEach((headerText) => {
+        const header = document.createElement("th");
         header.appendChild(document.createTextNode(headerText));
         headerRow.appendChild(header);
       });
       table.appendChild(headerRow);
-      data.forEach(rowData => {
-        const row = document.createElement('tr');
-        headers.forEach(headerText => {
-          const cell = document.createElement('td');
+      data.forEach((rowData) => {
+        const row = document.createElement("tr");
+        headers.forEach((headerText) => {
+          const cell = document.createElement("td");
           cell.appendChild(document.createTextNode(rowData[headerText]));
           row.appendChild(cell);
         });
@@ -25,4 +26,38 @@ function getData() {
 }
 
 getData();
+
+
+document.getElementById("file-input").addEventListener("change", function (e) {
+  const file = e.target.files[0];
+  const formData = new FormData();
+  formData.append("file", file);
+
+  fetch("http://localhost:5500/upload", {
+    method: "POST",
+    body: formData,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const table = document.getElementById("csvTable");
+      table.innerHTML = ""; // clear the contents of the table
+      const headers = Object.keys(data[0]);
+      const headerRow = document.createElement("tr");
+      headers.forEach((headerText) => {
+        const header = document.createElement("th");
+        header.appendChild(document.createTextNode(headerText));
+        headerRow.appendChild(header);
+      });
+      table.appendChild(headerRow);
+      data.forEach((rowData) => {
+        const row = document.createElement("tr");
+        headers.forEach((headerText) => {
+          const cell = document.createElement("td");
+          cell.appendChild(document.createTextNode(rowData[headerText]));
+          row.appendChild(cell);
+        });
+        table.appendChild(row);
+      });
+    });
+});
 
