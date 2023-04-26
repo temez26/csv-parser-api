bc2ce835071f8727648a29ef6753e04415cafbf0
Teemu Kalmari <teemu.kalmari@student.lab.fi>
2023-04-26 15:24:23 +0300

    error handling for ensuring that csv file is valid

--- main.js
+++ main.js
@@ -1,39 +1,47 @@
 
-
 const express = require("express");
 const csv = require("csv-parser");
 const fs = require("fs");
 const cors = require("cors");
 const app = express();
-const port = 5500;
+const port = process.env.PORT || 5500;
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+const upload = multer({ dest: process.env.UPLOAD_DEST || "uploads/" });
 
 // cors
 app.use(cors());
 
 let csvData = [];
 
-app.post("/upload", upload.single("file"), (req, res, next) => {
-  const results = [];
-  const csvFilePath = req.file.path;
+app.post("/upload", upload.single("file"), async (req, res, next) => {
+  try {
+    const results = [];
+    const csvFilePath = req.file.path;
 
-  const readStream = fs.createReadStream(csvFilePath);
+    // Validate file type
+    if (req.file.mimetype !== "text/csv") {
+      throw new Error("Invalid file type. Please upload a CSV file.");
+    }
 
-  // Handle file read errors
-  readStream.on("error", (err) => {
-    console.error(err);
+    const readStream = fs.createReadStream(csvFilePath);
+
+    // Handle file read errors
+    readStream.on("error", (err) => {
+      console.error(err);
+      next(err);
+    });
+
+    readStream
+      .pipe(csv())
+      .on("data", (data) => results.push(data))
+      .on("end", () => {
+        csvData = results;
+        res.send(results);
+      })
+      .on("error", (err) => next(err));
+  } catch (err) {
     next(err);
-  });
-
-  readStream
-    .pipe(csv())
-    .on("data", (data) => results.push(data))
-    .on("end", () => {
-      csvData = results;
-      res.send(results);
-    })
-    .on("error", (err) => next(err));
+  }
 });
 
 app.get("/", (req, res) => {
@@ -43,4 +51,3 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Server running on port ${ port } `);
 });
-
