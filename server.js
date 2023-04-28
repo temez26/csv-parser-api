5ededce38c9e169e8ad9983be4b3d918600a4c15
Teemu Kalmari <95919515+temez26@users.noreply.github.com>
2023-04-28 18:35:24 +0300

    added comments

--- server.js
+++ server.js
@@ -8,11 +8,12 @@ const port = process.env.PORT || 5500;
 const multer = require("multer");
 const upload = multer({ dest: process.env.UPLOAD_DEST || "uploads/" });
 
-// cors
+// Enable Cross-Origin Resource Sharing (CORS)
 app.use(cors());
 
 let csvData = [];
 
+// POST route for uploading a CSV file
 app.post("/upload", upload.single("file"), async (req, res, next) => {
   try {
     const results = [];
@@ -31,6 +32,7 @@ app.post("/upload", upload.single("file"), async (req, res, next) => {
       next(err);
     });
 
+    // Parse CSV data and store in memory
     readStream
       .pipe(csv())
       .on("data", (data) => results.push(data))
@@ -44,10 +46,12 @@ app.post("/upload", upload.single("file"), async (req, res, next) => {
   }
 });
 
+// GET route for retrieving parsed CSV data
 app.get("/", (req, res) => {
   res.json(csvData);
 });
 
+// Start server and listen on specified port
 app.listen(port, () => {
   console.log(`Server running on port ${ port } `);
 });
